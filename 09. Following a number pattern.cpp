745166dfd8606ed3d85e7b83ece53a626ae2cff2
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-01-04 11:43:20 +0530

    Update 09. Following a number pattern.cpp

--- 09. Following a number pattern.cpp
+++ 09. Following a number pattern.cpp
@@ -1,32 +1,38 @@
+/*
+Problem Statement:
 
-
+Given a pattern containing only I's and D's. I for increasing and D for decreasing. Devise an algorithm 
+to print the minimum number following that pattern. Digits from 1-9 and digits can't repeat.
+*/   
 
 //Implementation
 
 class Solution{   
 public:
     string printMinNumberForPattern(string S){
-        // code here 
+        
         string ans="";
-        for(int i=1;i<=S.size()+1;i++){
-            ans+= '0'+i;
-        }
+        for(int i=1;i<=S.size()+1;i++) ans+= '0'+ i;
+        
         while(true){
             int j=1,k=0;
             bool f=true;
+            
             while(j<ans.size()){
                if(S[k]=='D'&& ans[j] > ans[j-1]){
                 swap(ans[j],ans[j-1]);
                 f=false;
               } 
+              
               if(S[k]=='I' && ans[j] < ans[j-1]){
                   swap(ans[j],ans[j-1]);
                   f=false;
               }
-              j++;
-              k++;
+    
+              j++; k++;
             }
-            if(f==true)break;
+            
+            if(f==true) break;
         }
         return ans;
     }
