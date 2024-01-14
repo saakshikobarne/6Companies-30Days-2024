720fb2218ed34efeffef442cc3b481f9dd982bd7
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-01-14 22:38:53 +0530

    Update 34. K-diff pairs in an array.cpp

--- 34. K-diff pairs in an array.cpp
+++ 34. K-diff pairs in an array.cpp
@@ -13,3 +13,25 @@ Notice that |val| denotes the absolute value of val.
 
 //code:
 
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int ans = 0, i = 0, j = 1;
+
+        for(i, j; i<nums.size() && j<nums.size(); ) {
+            
+            if(i==j || nums[j]-nums[i]<k) j++;
+            else {
+                if(nums[j]-nums[i]==k) {
+                    ans++;
+                    j++;
+                    while(j<nums.size() && nums[j]==nums[j-1]) j++; 
+                }
+                i++;
+                while(i<j && nums[i]==nums[i-1]) i++; 
+            }
+        }
+        return ans;
+    }
+};
