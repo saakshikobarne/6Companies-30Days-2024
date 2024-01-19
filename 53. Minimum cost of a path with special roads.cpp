550bfc0e6ddbe343c22365c876244bdca65f8bbf
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-01-19 10:21:49 +0530

    Create 53. Minimum cost of a path with special roads.cpp

---
+++ 53. Minimum cost of a path with special roads.cpp
@@ -0,0 +1,17 @@
+/*
+Problem statement:
+
+You are given an array start where start = [startX, startY] represents your initial position (startX, startY) in a 2D space. 
+You are also given the array target where target = [targetX, targetY] represents your target position (targetX, targetY).
+
+The cost of going from a position (x1, y1) to any other position in the space (x2, y2) is |x2 - x1| + |y2 - y1|.
+
+There are also some special roads. You are given a 2D array specialRoads where specialRoads[i] = [x1i, y1i, x2i, y2i, costi] 
+indicates that the ith special road can take you from (x1i, y1i) to (x2i, y2i) with a cost equal to costi. You can use each 
+special road any number of times.
+
+Return the minimum cost required to go from (startX, startY) to (targetX, targetY).
+*/
+
+//code:
+
