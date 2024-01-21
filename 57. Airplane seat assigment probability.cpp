314ffdcd8d0f51a754cebcfe38581cc593a8c3f1
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-01-21 22:47:02 +0530

    Create 57. Airplane seat assigment probability.cpp

---
+++ 57. Airplane seat assigment probability.cpp
@@ -0,0 +1,17 @@
+/*
+n passengers board an airplane with exactly n seats. The first passenger has lost the ticket and picks a seat randomly. 
+But after that, the rest of the passengers will:
+
+Take their own seat if it is still available, and
+Pick other seats randomly when they find their seat occupied
+Return the probability that the nth person gets his own seat.
+*/
+
+//code:
+
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+       return (n==1)? 1.0 : 0.5;
+    }
+};
