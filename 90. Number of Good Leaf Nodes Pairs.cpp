d9274732659b043758b3c14ccb475b8b21d81a6e
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-02-01 23:24:36 +0530

    Create 90. Number of Good Leaf Nodes Pairs.cpp

---
+++ 90. Number of Good Leaf Nodes Pairs.cpp
@@ -0,0 +1,49 @@
+/*
+You are given the root of a binary tree and an integer distance. A pair of two different leaf nodes of a binary tree is said to be good if the length 
+of the shortest path between them is less than or equal to distance.
+
+Return the number of good leaf node pairs in the tree.
+*/
+
+//code:
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countPairs(TreeNode* root, int distance) {
+        int res = 0;
+        dfs(root, distance, res);
+        return res;
+    }
+    vector<int> dfs(TreeNode* root, int distance, int& res) {
+        if (!root->left && !root->right) return {1};
+        vector<int> left, right;
+        if (root->left) left = dfs(root->left, distance, res);
+        if (root->right) right = dfs(root->right, distance, res);
+        for (int i = 0; i < left.size(); ++i) {
+            for (int j = 0; j < right.size(); ++j) {
+                if (left[i] + right[j] <= distance) ++res;
+            }
+        }
+        vector<int> res_vec;
+        for (int i = 0; i < left.size(); ++i) {
+            if (left[i] + 1 < distance) res_vec.push_back(left[i] + 1);
+        }
+        for (int i = 0; i < right.size(); ++i) {
+            if (right[i] + 1 < distance) res_vec.push_back(right[i] + 1);
+        }
+        return res_vec;
+    }
+};
